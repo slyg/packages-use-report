454a924ff1411f0fb16bc43d0289d69cd641f269
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-09-24 16:35:39 +0100

    Add check for helmet in node apps gathering

--- bin/report_node_apps.py
+++ bin/report_node_apps.py
@@ -5,8 +5,10 @@ import numpy as np
 from utils import get_repos_for_code_search, save_csv
 
 OUTPUT_FILE = "reports/node-apps-report.csv"
-QUERY_WITH_EXPRESS = "org:hmcts+path:/+filename:package.json+express"
+
 QUERY_ALL_JS = "org:hmcts+path:/+filename:package.json"
+QUERY_WITH_EXPRESS = "org:hmcts+path:/+filename:package.json+express"
+QUERY_WITH_HELMET = "org:hmcts+path:/+filename:package.json+helmet"
 QUERY_NODE_BASED_DOCKER_IMAGE = "org:hmcts+path:/+filename:Dockerfile+node"
 QUERY_NODE_BASED_DOCKER_IMAGE_2 = "org:hmcts+path:/+filename:Dockerfile+hmctspublic.azurecr.io/base/node"
 
@@ -16,6 +18,8 @@ def main():
     js_projects = np.array(list(get_repos_for_code_search(QUERY_ALL_JS)))
     js_projects_with_express = np.array(
         list(get_repos_for_code_search(QUERY_WITH_EXPRESS)))
+    js_projects_with_helmet = np.array(
+        list(get_repos_for_code_search(QUERY_WITH_HELMET)))
 
     node_based_docker_projects = np.array(
         list(get_repos_for_code_search(QUERY_NODE_BASED_DOCKER_IMAGE)))
@@ -28,6 +32,7 @@ def main():
     all_concatenated = np.concatenate((
         js_projects,
         js_projects_with_express,
+        js_projects_with_helmet,
         concatenated_node_based_docker_projects
     ))
 
@@ -36,7 +41,7 @@ def main():
     all_projects_sorted = sorted(all_projects, key=lambda item: item['name'])
 
     fieldnames = ['Reference', 'Uses Express',
-                  'Node-based docker image', 'Likely nodejs app']
+                  'Node-based docker image', 'Likely nodejs app', 'Use Helmet']
 
     def likely_node_app(item):
         return (item in concatenated_node_based_docker_projects) or (item in js_projects_with_express)
@@ -45,7 +50,8 @@ def main():
         fieldnames[0]: item['name'],
         fieldnames[1]: 'X' if (item in js_projects_with_express) else '',
         fieldnames[2]: 'X' if (item in concatenated_node_based_docker_projects) else '',
-        fieldnames[3]: 'X' if likely_node_app(item) else ''
+        fieldnames[3]: 'X' if likely_node_app(item) else '',
+        fieldnames[4]: 'X' if (item in js_projects_with_express) else '',
     }, all_projects_sorted))
 
     save_csv(OUTPUT_FILE, fieldnames, rows)
